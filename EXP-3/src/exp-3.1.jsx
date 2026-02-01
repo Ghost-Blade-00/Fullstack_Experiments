6c356daf7c6a04b045543c9b278306050a166213
Aditya Yadav <aditech0246@gmail.com>
2026-02-01 21:08:39 +0530

    Added Experiment 3 source files (React routing)

---
+++ EXP-3/src/exp-3.1.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function About() {
+  return <h1>About Page</h1>;
+}
+
+export default function Exp31() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
